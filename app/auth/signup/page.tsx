49814e0dd6c6d5ff4f062cc5c43e8e0b67b64c5f
ahmadkhalid47 <you@example.com>
2025-05-14 03:52:48 +0500

    erorr

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,5 +1,6 @@
-import { SignupForm } from "@/components/auth/signup-form"
-import Link from "next/link"
+"use client";
+import { SignupForm } from "@/components/auth/signup-form";
+import Link from "next/link";
 
 export default function SignupPage() {
   return (
@@ -9,17 +10,24 @@ export default function SignupPage() {
       </Link>
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">Create an account</h1>
-          <p className="text-sm text-muted-foreground">Enter your information to create a user account</p>
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Create an account
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your information to create a user account
+          </p>
         </div>
         <SignupForm />
         <p className="px-8 text-center text-sm text-muted-foreground">
           Already have an account?{" "}
-          <Link href="/auth/login" className="underline underline-offset-4 hover:text-primary">
+          <Link
+            href="/auth/login"
+            className="underline underline-offset-4 hover:text-primary"
+          >
             Sign in
           </Link>
         </p>
       </div>
     </div>
-  )
+  );
 }
