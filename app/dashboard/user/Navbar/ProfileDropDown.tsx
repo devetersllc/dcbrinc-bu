298e69edbdfc0ef47ec24a7ab035c60ab573a7ae
ahmadkhalid47 <ahmadrazakhalid9.0@gmail.com>
2025-07-06 00:47:55 +0500

    test

--- app/dashboard/user/Navbar/ProfileDropDown.tsx
+++ app/dashboard/user/Navbar/ProfileDropDown.tsx
@@ -35,6 +35,7 @@ export default function ProfileDropDown() {
 
   return (
     <DropdownMenu>
+      
       <DropdownMenuTrigger asChild className="cursor-pointer">
         <Avatar>
           <AvatarImage src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
