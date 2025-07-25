4a13c115c31d90a0a00f96823ccdfab922a25d79
ahmadkhalid47 <ahmadrazakhalid9.0@gmail.com>
2025-07-26 03:25:43 +0500

    feat: Add Books page with service type dispatch and main tabs integration

---
+++ app/books/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useEffect } from "react";
+import MainTabs from "../dashboard/user/MainTabs";
+import { setServiceType } from "@/lib/features/general/general";
+import { useDispatch } from "react-redux";
+
+export default function Books() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setServiceType("books"));
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="container mx-auto px-4 py-8">
+        <MainTabs />
+      </div>
+    </div>
+  );
+}
